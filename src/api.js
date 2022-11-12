782b64aedce216918b1e57d3ec9b29b8fd0a3231
Eddiemetrium <eddiemitch262@gmail.com>
2022-11-12 13:52:18 -0800

    all done...

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "1997d8599cmsh827949bd71a3846p174abdjsn0f6f130e3de0",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "0bfe8f94917a6317dfde5c259f17aaf2"; // enter your key from openweather API
+// console.log(geoApiOptions, GEO_API_URL, WEATHER_API_URL, WEATHER_API_KEY);
